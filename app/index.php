4130dee40dce5b870f17a9362ccb8b13f26b1fed
cdiaz <cdiaz@quantux.co>
2016-07-26 03:11:55 -0500

    first commit

---
+++ app/index.php
@@ -0,0 +1,23 @@
+<?php
+phpinfo();
+$manager = new MongoDB\Driver\Manager("mongodb://becomp_mongodb:27017");
+
+$bulk = new MongoDB\Driver\BulkWrite;
+$bulk->insert(['x' => 1]);
+$bulk->insert(['x' => 2]);
+$bulk->insert(['x' => 3]);
+$manager->executeBulkWrite('db.collection', $bulk);
+
+$filter = ['x' => ['$gt' => 1]];
+$options = [
+    'projection' => ['_id' => 0],
+    'sort' => ['x' => -1],
+];
+
+$query = new MongoDB\Driver\Query($filter, $options);
+$cursor = $manager->executeQuery('db.collection', $query);
+
+foreach ($cursor as $document) {
+    var_dump($document);
+}
+?>
