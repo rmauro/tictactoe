a5cf682025787ee897c396315eb4d7b093aab96c
Rafael Mauro <rmauro@gmail.com>
2019-04-03 00:14:11 -0300

    Improve structure to PHP application dev
    - Move composer.json file to /app directory
    - Configure phpunit and bootstrap tests

---
+++ app/tests/bootstrap.php
@@ -0,0 +1,12 @@
+<?php
+
+require __DIR__ . '/../vendor/autoload.php';
+/*
+$settings = require __DIR__ . '/../settings.php';
+$app = new \Slim\App($settings);
+
+require __DIR__ . '/../dependencies.php';
+require __DIR__ . '/../app/routes.php';
+
+$app->run();
+*/
